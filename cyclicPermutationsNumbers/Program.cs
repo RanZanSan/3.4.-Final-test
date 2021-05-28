f141cbdddf16175a1dd51af17184c948955ee2bf
Andrey Rasskazov <ranzansan@gmail.com>
2021-05-28 17:46:36 +0300

    update

--- cyclicPermutationsNumbers/Program.cs
+++ cyclicPermutationsNumbers/Program.cs
@@ -11,11 +11,18 @@ namespace cyclicPermutationsNumbers
         static void Main(string[] args)
         {
             int number = Convert.ToInt32(Console.ReadLine());
-
             int fourthNumber = number % 10;
             int thirdNumber = (number % 100) / 10;
             int secondNumber = (number % 1000) / 100;
             int firstNumber = (number % 10000) / 1000;
+
+            int cyclicPermutationsOne = fourthNumber * 1000 + firstNumber * 100 + secondNumber * 10 + thirdNumber;
+            int cyclicPermutationsTwo = secondNumber * 1000 + thirdNumber * 100 + fourthNumber * 10 + firstNumber;
+            int cyclicPermutationsThree = thirdNumber * 1000 + fourthNumber * 100 + firstNumber * 10 + secondNumber;
+            double cyclicPermutationsMax = Math.Max(Math.Max(number, cyclicPermutationsOne), Math.Max(cyclicPermutationsTwo, cyclicPermutationsThree));
+
+            Console.WriteLine(cyclicPermutationsMax);
+            Console.ReadKey();
         }
     }
 }
--- numberPermutation/Program.cs
+++ numberPermutation/Program.cs
@@ -11,21 +11,21 @@ namespace numberPermutation
         static void Main(string[] args)
         {
             int number = Convert.ToInt32(Console.ReadLine());
-
             int fourthNumber = number % 10;
             int thirdNumber = (number % 100) / 10;
             int secondNumber = (number % 1000) / 100;
             int firstNumber = (number % 10000) / 1000;
 
-            double firstNumbersMax = Math.Max(Math.Max(firstNumber, secondNumber), Math.Max(thirdNumber, fourthNumber));
-            double secondNumbersMax = Math.Min(Math.Max(Math.Min(firstNumber, secondNumber), Math.Max(thirdNumber, fourthNumber)), Math.Max(Math.Min(firstNumber, secondNumber), Math.Max(thirdNumber, fourthNumber)));
-            double thirdNumbersMax = Math.Max(Math.Min(Math.Max(firstNumber, secondNumber), Math.Min(thirdNumber, fourthNumber)), Math.Min(Math.Min(firstNumber, secondNumber), Math.Max(thirdNumber, fourthNumber)));
-            double fourthNumbersMin = Math.Min(Math.Min(firstNumber, secondNumber), Math.Min(thirdNumber, fourthNumber));
+            double firstMax = Math.Max(Math.Max(firstNumber, secondNumber), Math.Max(thirdNumber, fourthNumber));
+            double secondMin = Math.Min(Math.Max(firstNumber, thirdNumber), Math.Max(secondNumber, fourthNumber));
+            double thirdMax = Math.Max(Math.Min(firstNumber, thirdNumber), Math.Min(secondNumber, fourthNumber));
+            double fourthMin = Math.Min(Math.Min(firstNumber, secondNumber), Math.Min(thirdNumber, fourthNumber));
 
-            Console.Write(firstNumbersMax);
-            Console.Write(secondNumbersMax);
-            Console.Write(thirdNumbersMax);
-            Console.Write(fourthNumbersMin);
+            Console.Write(firstMax);
+            Console.Write(secondMin);
+            Console.Write(thirdMax);
+            Console.Write(fourthMin);
+            Console.ReadKey();
         }
     }
 }
